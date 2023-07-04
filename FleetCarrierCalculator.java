10c134c75d0b4ad22eebca5b69025dafbd88e4ed
Greymagic27 <tolly67p@gmail.com>
2023-07-04 11:56:10 +0100

    Typo

--- FleetCarrierCalculator.java
+++ FleetCarrierCalculator.java
@@ -8,8 +8,8 @@ public class FleetCarrierCalculator {
 
     private static final long COLONIA_TRITIUM = 270086 * 2000; // 270086 = Buy price // 2000 = 2000 tonnes to get to GA
     private static final long GA_TRITIUM = 150007 * 5000; // 150007 = Buy price // 5000 = 5000 tonnes to get around
-    private static final long T9_STORTED_TRITIUM = 150007 * 752; // 50007 = Buy price // 752 = 752 tonnes to store in T9
-    private static final long TRITIUM_COST = COLONIA_TRITIUM + GA_TRITIUM + T9_STORTED_TRITIUM;
+    private static final long T9_STORED_TRITIUM = 150007 * 752; // 50007 = Buy price // 752 = 752 tonnes to store in T9
+    private static final long TRITIUM_COST = COLONIA_TRITIUM + GA_TRITIUM + T9_STORED_TRITIUM;
 
     private static final long CARRIER_COST = 5445000000L;
     private static final long CREDITS_DUMPED_INTO_CARRIER = 1000000000; // 1 billion
