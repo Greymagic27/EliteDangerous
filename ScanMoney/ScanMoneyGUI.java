45fd63f085fe5d8ef0b09b1786d4bfa67116ca79
Greymagic27 <tolly67p@gmail.com>
2024-03-10 13:13:58 +0000

    Clean up imports

--- ScanMoney/ScanMoneyGUI.java
+++ ScanMoney/ScanMoneyGUI.java
@@ -1,9 +1,10 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
